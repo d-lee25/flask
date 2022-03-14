ee611d905772ca52a0fb0f98f0588dccb6403373
alee <andrew.lee-2@leidos.com>
2022-03-14 09:28:08 -0600

    adding html using basic jinja2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # All you need to start a flask app
 app = Flask(__name__)
@@ -6,10 +6,13 @@ app = Flask(__name__)
 # adding routes
 
 # you can add multiple routes for same
-#@app.route('/index') - both routes lead to same place
+@app.route('/index') # both routes lead to same place
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', current_title = 'Custom Title')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=True)
